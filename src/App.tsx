280074628e15249176ea9e90eb306ab97d5f30da
DeltaLF <patricktsaikl@gmail.com>
2022-09-19 23:07:03 +0800

    setup basic redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import './App.css';
+import { CssBaseline } from '@material-ui/core';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Route, } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
+import { QueryParamProvider } from 'use-query-params';
+import './index.css';
+import FullscreenProgress from './shared/components/FullscreenProgress/FullscreenProgress';
+import { persistor, store } from './shared/redux/store';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-App
-    </div>
+    <Provider store={store}>
+      <PersistGate loading={<FullscreenProgress />} persistor={persistor}>
+        <BrowserRouter>
+          <QueryParamProvider ReactRouterRoute={Route}>
+            <CssBaseline />
+          </QueryParamProvider>
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
   );
 }
 
